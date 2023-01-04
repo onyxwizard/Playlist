e20000ed6350856a032e0519fb88a17152b2cfe8
ak <67966017+onyxwizard@users.noreply.github.com>
2023-01-04 23:11:30 +0000

    [Done] Comments displayed based on post item

--- resources/views/poster/index.blade.php
+++ resources/views/poster/index.blade.php
@@ -344,7 +344,8 @@
                         </form>    
                       </div>
     
-    
+                      @foreach($comments as $it)
+                      {{$loop->iteration}}
                       <div class="mt-2">
     
                         <div class="d-flex flex-row p-3">
@@ -355,35 +356,24 @@
     
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="d-flex flex-row align-items-center">
-                                  <span class="c-badge">{{$item->user_post_name}}</span>
+                                  <span class="c-badge">{{$it->user_comment_name}}</span>
                                   
                                 </div>
-                                <small>12h ago</small>
+                                <small>{{$it->created}}</small>
                           </div>
     
-                          <p class="text-justify comment-text mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                          <p class="text-justify comment-text mb-0">{{$it->cbody}}</p>
     
                           <div class="d-flex flex-row user-feed">
     
                             <span class="wish"><i class="fa fa-heartbeat mr-2"></i>24</span>
                             <span class="ml-3"><i class="fa fa-comments-o mr-2"></i>Reply</span>
     
-                            
                           </div>
-                          
-                        </div>
-                        
-    
+                        </div>    
                       </div>
-    
-    
-                    
-    
-    
-                    
-                      
                     </div>
-                    
+                    @endforeach
                   </div>
                   
                 </div>
